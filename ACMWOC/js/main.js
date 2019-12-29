119d97907c8844e4f0c16229faad2766e8dec9e6
Sujay1101 <58305810+Sujay1101@users.noreply.github.com>
2019-12-29 20:56:42 +0530

    Update main.js

--- ACMWOC/js/main.js
+++ ACMWOC/js/main.js
@@ -1,3 +1,52 @@
+const ani_navbar = document.querySelectorAll('.nav-item , .btn_hover_two');
+const ani_head = document.querySelectorAll('h1,h2,h3,h4');
+const ani_text = document.querySelectorAll('p,.listItem , .view_btn');
+
+observer_navbar = new IntersectionObserver(entries =>{
+	entries.forEach(entry => {
+		if(entry.intersectionRatio > 0){
+			entry.target.className += " fadeIn second";
+		}
+	})
+})
+
+observer_head = new IntersectionObserver(entries =>{
+	entries.forEach(entry => {
+		if(entry.intersectionRatio > 0){
+			entry.target.className += " fadeInDown";
+		}
+	})
+})
+
+observer_text = new IntersectionObserver(entries =>{
+	entries.forEach(entry => {
+		if(entry.intersectionRatio > 0){
+			entry.target.className += " fadeInUp";
+		}
+	})
+})
+
+ani_navbar.forEach((ani_navbar) => {
+	observer_navbar.observe(ani_navbar)
+})
+
+ani_head.forEach((ani_head) => {
+	observer_head.observe(ani_head)
+})
+
+ani_text.forEach((ani_text) => {
+	observer_text.observe(ani_text)
+})
+
+let test = document.querySelector(".navbar-brand");
+test.addEventListener('mouseenter',function(event){
+	let image = event.target.querySelector('img') ;
+	image.style.height = "70px";
+	setTimeout(function(){
+		image.style.height = "50px";
+	},2500);
+});
+
 $(document).ready(function() {
 	// Header Scroll
 	$(window).on('scroll', function() {
